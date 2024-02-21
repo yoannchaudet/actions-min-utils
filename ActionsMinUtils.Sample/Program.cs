1f382cb245782a4ddb3efc9cb457c8054a9a60d3
Yoann Chaudet <c.yoann@gmail.com>
2024-02-20 21:05:38 -0800

    Add a basic integration test

---
+++ ActionsMinUtils.Sample/Program.cs
@@ -0,0 +1,13 @@
+﻿using ActionsMinUtils;
+
+var ctx = new Context();
+Logger.Debug($"Current workspace is {ctx.GitHubWorkspace}");
+using (Logger.Group("Greetings"))
+{
+    Logger.Info("Hello world!");
+}
+
+internal class Context : ActionContext
+{
+    public string GitHubWorkspace => GetEnvironmentVariable("GITHUB_WORKSPACE", true)!;
+}
