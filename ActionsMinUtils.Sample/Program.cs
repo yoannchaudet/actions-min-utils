4f2001e55ce611ebc98152ab764c10777edbbea1
Yoann Chaudet <c.yoann@gmail.com>
2024-02-24 13:34:23 -0800

    Make API calls in the sample (#23)
    Make API calls in the sample

--- ActionsMinUtils.Sample/Program.cs
+++ ActionsMinUtils.Sample/Program.cs
@@ -1,15 +1,27 @@
 ﻿using ActionsMinUtils;
 
+// Get stuff from the context
 var ctx = new Context();
+
+// Log a few things out of the context
 Logger.Debug($"Current workspace is {ctx.GitHubWorkspace}");
 Logger.Info($"Hello, {ctx.Name}!");
-using (Logger.Group("Greetings"))
+using (Logger.Group("Here is a little greetings group"))
 {
     Logger.Info("Hello world!");
 }
 
+// Make some API calls too
+var github = new GitHub(ctx.GitHubToken);
+var ownerName = ctx.GitHubRepository.Split('/')[0];
+var repoName = ctx.GitHubRepository.Split('/')[1];
+var repo = await github.ExecuteAsync(async () => await github.RestClient.Repository.Get(ownerName, repoName));
+Logger.Info($"This action ran from the context of repository {repo.Id} and the {repo.License.Name}!");
+
 internal class Context : ActionContext
 {
     public string GitHubWorkspace => GetEnvironmentVariable("GITHUB_WORKSPACE", true)!;
+    public string GitHubToken => GetEnvironmentVariable("GITHUB_TOKEN", true)!;
+    public string GitHubRepository => GetEnvironmentVariable("GITHUB_REPOSITORY", true)!;
     public string Name => GetInput("name", true)!;
 }
